be25c6e26c020d8d069d965a460df9ee133d2c15
Petr <petr.gulchuk@gmail.com>
2018-11-15 01:18:04 +0100

    applyied cors rules

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import properties from "./properties";
 import index from "./controller/index";
@@ -7,10 +8,15 @@ import models from "./model";
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', index);
+app.use('/', cors(corsOptions), index);
 
 models.sequelize.sync().then(function () {
   app.listen(properties.PORT, () => {
