fef0072f454c0942cbbaba3d3f360c96048afc55
Petr <petr.gulchuk@gmail.com>
2018-11-19 00:01:49 +0100

    fixed issue

--- server/controller/exchangeCompanyController.js
+++ server/controller/exchangeCompanyController.js
@@ -24,7 +24,7 @@ exchangeCompanyController.get('/', (req, res) => {
 
 // run scraper
 exchangeCompanyController.put('/scraper', (req, res) => {
-  Promise.all([scraper_2, scraper])
+  Promise.all([scraper_2(), scraper()])
     .then(responses => {
       const filteredResponses = responses.filter(response => response && Array.isArray(response));
       const concatedResponses = filteredResponses.reduce((responseAcc, response) => {
